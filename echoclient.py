3a6008910d7e3f7b5916e4710c56b53c1b9ea015
yeonwoo <yeonu1201@gmail.com>
2020-08-31 12:19:31 +0900

    file transfer system added

--- echoclient.py
+++ echoclient.py
@@ -1,18 +1,53 @@
 import socket
 
-HOST = '192.168.111.100'
+HOST = 'localhost'
 PORT = 9009
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
+while True:
+    service = input("""무슨 서비스를 원하십니까?
+'c': 채팅 시스템, 'f' 파일전송 : """)
 
-    while True:
-        msg = input("Message: ")
-        if msg == "/quit":
-            sock.sendall(msg.encode())
-            break
+    if service == 'c':
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            service = "chat"
+            sock.send(service.encode()) # 서비스 종류를 알려줌
 
-        sock.send(msg.encode())
-        answer = sock.recv(1024)
-        print("From server: %s" %answer.decode())
+            while True:
+                msg = input("Message: ")
+                if msg == "/quit":
+                    sock.sendall(msg.encode())
+                    break
 
+                sock.send(msg.encode())
+                answer = sock.recv(1024)
+                print("From server: %s" %answer.decode())
+    elif service == 'f':
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            service = "file_transfer"
+            sock.send(service.encode()) # 서비스 종류를 알려줌
+
+            data_transfer = 0   # 서버로 부터 받아온 파일 크기
+
+            filename = input("받을 파일의 이름: ")    # 나중에 함수로 다시 쓸때는 파라미터로 가지고 올 것
+            sock.send(filename.encode())
+
+            data = sock.recv(1024)
+            if not data:
+                print("파일 [%s]: 서버에 존재하지 않거나 전송중 오류발생" %filename)
+                # return
+
+            with open("repo/client/" + filename, 'wb') as file:
+                try:
+                    while data:
+                        file.write(data)
+                        data_transfer += len(data)
+                        data = sock.recv(1024)
+                except Exception as e:
+                    print(e)
+
+            print("파일 [%s] 전송완료. 전송량 [%d] Bytes" %(filename, data_transfer))
+
+    else:
+        print("c 또는 f를 입력하세요")
--- echoserver_forever.py
+++ echoserver_forever.py
@@ -1,26 +1,61 @@
 import socketserver
+from os.path import exists
+import os
 
 HOST = ''
 PORT = 9009
 
 class MyTcpHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        print("[%s] 연결됨!" %self.client_address[0])
-        try:
-            while True:
-                self.data = self.request.recv(1024)
-                if self.data.decode() == '/quit':
-                    print("[%s] 사용자에 의해 중단" %self.client_address[0])
-                    return
-                # /quit 메세지가 아닌 이상 계속해서 echo
-                print("[%s] %s" %(self.client_address[0], self.data.decode()))
-                self.request.sendall(self.data)
+        self.service = self.request.recv(1024).decode()
 
-        except Exception as e:
-            print(e)
+        # 채팅 서비스를 진행할 코드
+        if self.service == "chat":
+            print("채팅 시스템에 [%s] 연결됨!" %self.client_address[0])
+            try:
+                while True:
+                    self.data = self.request.recv(1024)
+                    if self.data.decode() == '/quit':
+                        print("[%s] 사용자에 의해 중단" %self.client_address[0])
+                        return
+                    # /quit 메세지가 아닌 이상 계속해서 echo
+                    print("[%s] %s" %(self.client_address[0], self.data.decode()))
+                    self.request.sendall(self.data)
+
+            except Exception as e:
+                print(e)
+
+        # 파일 송신 프로그램
+        if self.service == "file_transfer":
+            data_transferred = 0
+            print("파일 전송 시스템에 [%s] 연결됨!" %self.client_address[0])
+
+            filename = self.request.recv(1024).decode()
+            server_file_path = os.getcwd() + "\\repo\\server\\" + filename
+            # print(server_file_path)
+
+
+            if not exists(server_file_path):
+                print("[%s] 존재하지 않음!" %server_file_path)
+                return
+
+            print("[%s] 전송 시작..." %filename)
+
+            with open(server_file_path, 'rb') as file:
+                try:
+                    data = file.read(1024)  # file.read("byte") byte씩 읽는다. 넣지않을경우엔 한번에 다 읽는다.
+                    while data:
+                        data_transferred += self.request.send(data)
+                        data = file.read(1024)
+
+                except Exception as e:
+                    print(e)
+
+            print("전송 완료! [%d]Bytes" %data_transferred)
+
 
 def runServer():
-    print("+++Echo server is started")
+    print("+++Echo/file transfer server is started")
     try: # TCP를 사용하며 HOST, PORT로 바인딩된 객체를 생성해 server로 둔다
         server = socketserver.TCPServer((HOST, PORT), MyTcpHandler)
         server.serve_forever() # Interrupt하기 전까지 계속 실행
